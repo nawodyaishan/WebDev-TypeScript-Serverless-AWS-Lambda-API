ad0dcedd02c1f2b1088b2c831b643ffad9274d7a
nawodyaishan <nawodyain@gmail.com>
2022-10-21 23:57:11 +0530

    Update

--- src/libs/handler-resolver.ts
+++ src/libs/handler-resolver.ts
@@ -1,3 +1,5 @@
 export const handlerPath = (context: string) => {
-  return `${context.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}`
+
+    return `${context.split(process.cwd())[1].substring(1).replace(/\\/g, '/')}`
+
 };
